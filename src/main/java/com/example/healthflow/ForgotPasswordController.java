e44574e35b5d9b536ca69d8bbceb180823ce5e2e
Harshal Bhosale <134089001+codehb01@users.noreply.github.com>
2024-09-22 23:59:42 +0530

    New changes

---
+++ src/main/java/com/example/healthflow/ForgotPasswordController.java
@@ -0,0 +1,35 @@
+package com.example.healthflow;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+
+public class ForgotPasswordController {
+
+
+    @FXML
+    public AnchorPane ankrMain;
+    @FXML
+    public Button btnBack;
+
+    @FXML
+    public Button btnClearAll;
+
+    @FXML
+    public Button btnResetPass;
+
+    @FXML
+    public PasswordField passfConfirmNewPass;
+
+    @FXML
+    public PasswordField passfNewPass;
+
+    @FXML
+    public TextField txtfSecurityAnswer;
+}
+
+
